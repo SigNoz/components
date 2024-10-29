fff8c8713a8a7a2d0ac1e4ef8fb52a99c0ed160a
ahmadshaheer <ashaheerki@gmail.com>
2024-10-29 19:56:11 +0430

    feat: rewrite pagination component

--- packages/pagination/vite.config.ts
+++ packages/pagination/vite.config.ts
@@ -1,31 +1,42 @@
-import path from "path";
-import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
-import { libInjectCss } from "vite-plugin-lib-inject-css";
+import path from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
 
 export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, "src/pagination.tsx"),
-      name: "Button",
-      fileName: "pagination",
-    },
-    rollupOptions: {
-      external: ["react"],
-      output: {
-        globals: {
-          react: "React",
-        },
-      },
-      treeshake: true,
-    },
-  },
-  plugins: [dts(), react(), libInjectCss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+	build: {
+		lib: {
+			entry: resolve(__dirname, 'src/index.ts'),
+			formats: ['es', 'cjs'],
+			fileName: (format) => `index.${format}.js`,
+		},
+		rollupOptions: {
+			external: ['react', 'react/jsx-runtime'],
+			output: {
+				globals: {
+					react: 'React',
+					'react/jsx-runtime': 'jsx',
+				},
+			},
+			treeshake: true,
+		},
+	},
+	plugins: [
+		dts({
+			tsconfigPath: './tsconfig.json',
+			outDir: 'dist',
+			exclude: ['**/*.test.ts', '**/*.test.tsx'],
+			rollupTypes: true,
+			insertTypesEntry: true,
+		}),
+		react(),
+		libInjectCss(),
+	],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
 });
