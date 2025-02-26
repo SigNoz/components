08810f6df85f4b2ad591ff2286913612fc18c044
ahmadshaheer <ashaheerki@gmail.com>
2025-02-26 12:37:37 +0430

    chore: testing button component in packages-playground

--- apps/packages-playground/src/App.tsx
+++ apps/packages-playground/src/App.tsx
@@ -1,33 +1,42 @@
-import "./App.css";
-import { ThemeSwitcher, useTheme } from "@signozhq/theme";
-import { Button } from "@signozhq/button";
-import { Typography } from "@signozhq/design-tokens";
-import { Spacing } from "@signozhq/design-tokens";
+import './App.css';
+
+import { ThemeSwitcher, useTheme } from '@signozhq/theme';
+import Button from '@signozhq/button';
+import { Typography } from '@signozhq/design-tokens';
+import { Spacing } from '@signozhq/design-tokens';
+import { ColorTailwind } from '@signozhq/design-tokens';
+import { ArrowBigRightDash, Wrench } from 'lucide-react';
 
 function App() {
-  const { theme, currentThemeColors } = useTheme();
+	const { currentThemeColors } = useTheme();
+	console.log(ColorTailwind);
 
-  return (
-    <>
-      <h1>Theme Switcher and Button</h1>
-      <ThemeSwitcher />
+	return (
+		<>
+			<h1>Theme Switcher and Button</h1>
+			<ThemeSwitcher />
 
-      <Button theme={theme} variant="secondary">
-        hey
-      </Button>
-
-      <h1>Design tokens</h1>
-      <p
-        style={{
-          background: currentThemeColors.TEXT_COLOR,
-          letterSpacing: Spacing.MARGIN_10,
-          fontSize: Typography.FONTSIZE_3XL,
-        }}
-      >
-        hey
-      </p>
-    </>
-  );
+			<Button
+				backgroundColor="robin-500"
+				textColor="vanilla-100"
+				size="md"
+				prefixIcon={<Wrench />}
+				suffixIcon={<ArrowBigRightDash />}
+			>
+				Click Me
+			</Button>
+			<h1>Design tokens</h1>
+			<p
+				style={{
+					background: currentThemeColors.TEXT_COLOR,
+					letterSpacing: Spacing.MARGIN_10,
+					fontSize: Typography.FONTSIZE_3XL,
+				}}
+			>
+				hey
+			</p>
+		</>
+	);
 }
 
 export default App;
