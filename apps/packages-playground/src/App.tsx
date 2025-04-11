295c38aab214b224a3645b88d8e30d06ddbcafa3
ahmadshaheer <ashaheerki@gmail.com>
2025-04-11 13:27:38 +0430

    chore: add mode / theme UI to theme switcher component and consume it in packages-playground

--- apps/packages-playground/src/App.tsx
+++ apps/packages-playground/src/App.tsx
@@ -1,33 +1,17 @@
-import "./App.css";
-import { ThemeSwitcher, useTheme } from "@signozhq/theme";
-import { Button } from "@signozhq/button";
-import { Typography } from "@signozhq/design-tokens";
-import { Spacing } from "@signozhq/design-tokens";
+import React from 'react';
+
+import { Button } from '@signozhq/button';
+import './App.css';
+import { ThemeSwitcher } from '@signozhq/theme';
 
 function App() {
-  const { theme, currentThemeColors } = useTheme();
-
-  return (
-    <>
-      <h1>Theme Switcher and Button</h1>
-      <ThemeSwitcher />
-
-      <Button theme={theme} variant="secondary">
-        hey
-      </Button>
-
-      <h1>Design tokens</h1>
-      <p
-        style={{
-          background: currentThemeColors.TEXT_COLOR,
-          letterSpacing: Spacing.MARGIN_10,
-          fontSize: Typography.FONTSIZE_3XL,
-        }}
-      >
-        hey
-      </p>
-    </>
-  );
+	return (
+		<div>
+			<h1>Theme Switcher and Button</h1>
+			<ThemeSwitcher />
+			<Button variant="primary">hey</Button>
+		</div>
+	);
 }
 
 export default App;
