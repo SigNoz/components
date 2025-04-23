fee829d5ebdff80f9312d126a7b285bf7693e3fb
ahmadshaheer <ashaheerki@gmail.com>
2025-04-23 12:21:16 +0430

    refactor: chore: refactor button component to support primary / secondary / ... colors

--- packages/button/src/button.tsx
+++ packages/button/src/button.tsx
@@ -16,14 +16,11 @@ export interface ButtonProps
 }
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-	(
-		{ className, variant, size, theme = 'light', asChild = false, ...props },
-		ref,
-	) => {
+	({ className, variant, size, asChild = false, ...props }, ref) => {
 		const Comp = asChild ? Slot : 'button';
 		return (
 			<Comp
-				className={cn(buttonVariants({ variant, size, theme, className }))}
+				className={cn(buttonVariants({ variant, size, className }))}
 				ref={ref}
 				{...props}
 			/>
