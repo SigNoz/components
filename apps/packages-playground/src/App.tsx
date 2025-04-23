6fd78b82a30c8a45c65738291afafbf0b637c953
Aditya Singh <adityasingh@Adityas-MacBook-Pro.local>
2025-04-23 13:55:45 +0530

    feat: radio group init

--- apps/packages-playground/src/App.tsx
+++ apps/packages-playground/src/App.tsx
@@ -1,33 +1,48 @@
-import "./App.css";
-import { ThemeSwitcher, useTheme } from "@signozhq/theme";
-import { Button } from "@signozhq/button";
-import { Typography } from "@signozhq/design-tokens";
-import { Spacing } from "@signozhq/design-tokens";
+import './App.css';
+import { ThemeSwitcher, useTheme } from '@signozhq/theme';
+import { Button } from '@signozhq/button';
+import { Switch } from '@signozhq/switch';
+import { RadioGroup, RadioGroupItem } from '@signozhq/radio-group';
+
+import { Typography } from '@signozhq/design-tokens';
+import { Spacing } from '@signozhq/design-tokens';
 
 function App() {
-  const { theme, currentThemeColors } = useTheme();
+	const { theme, currentThemeColors } = useTheme();
 
-  return (
-    <>
-      <h1>Theme Switcher and Button</h1>
-      <ThemeSwitcher />
+	return (
+		<>
+			<h1>Theme Switcher and Button</h1>
+			<ThemeSwitcher />
 
-      <Button theme={theme} variant="secondary">
-        hey
-      </Button>
+			<Button theme={theme} variant="primary">
+				hey
+			</Button>
+			<Switch id="test" labelName={'Airplane mode'} disabled={false} />
+			<Switch id="test" labelName={'Airplane mode'} disabled={false} />
 
-      <h1>Design tokens</h1>
-      <p
-        style={{
-          background: currentThemeColors.TEXT_COLOR,
-          letterSpacing: Spacing.MARGIN_10,
-          fontSize: Typography.FONTSIZE_3XL,
-        }}
-      >
-        hey
-      </p>
-    </>
-  );
+			<RadioGroup defaultValue="option-one">
+				<div className="flex items-center space-x-2">
+					<RadioGroupItem value="option-one" id="option-one" />
+					<label htmlFor="option-one">Option One</label>
+				</div>
+				<div className="flex items-center space-x-2">
+					<RadioGroupItem value="option-two" id="option-two" />
+					<label htmlFor="option-two">Option Two</label>
+				</div>
+			</RadioGroup>
+			<h1>Design tokens</h1>
+			<p
+				style={{
+					background: currentThemeColors.TEXT_COLOR,
+					letterSpacing: Spacing.MARGIN_10,
+					fontSize: Typography.FONTSIZE_3XL,
+				}}
+			>
+				hey
+			</p>
+		</>
+	);
 }
 
 export default App;
