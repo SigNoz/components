366c87c7ad7a7fc2070e91a41b2b9a60dfda8e45
Aditya Singh <adityasingh@Adityas-MacBook-Pro.local>
2025-04-29 16:44:13 +0530

    chore: drawer init

---
+++ apps/docs/stories/drawer.stories.tsx
@@ -0,0 +1,31 @@
+// import type { Meta, StoryObj } from "@storybook/react";
+
+// const ExampleComponent = ({ text = "Example Component" }) => <h1>{text}</h1>;
+
+// const meta: Meta<typeof ExampleComponent> = {
+//   title: 'Components/Drawer',
+//   component: ExampleComponent,
+//   argTypes: {
+//     text: {
+//       control: { type: "text" },
+//     },
+//   },
+// };
+
+// export default meta;
+
+// type Story = StoryObj<typeof ExampleComponent>;
+
+// export const Default: Story = {
+//   render: (args) => <ExampleComponent {...args} />,
+//   args: {
+//     text: "Default Example",
+//   },
+// };
+
+// export const CustomText: Story = {
+//   render: (args) => <ExampleComponent {...args} />,
+//   args: {
+//     text: "Custom Example Text",
+//   },
+// };
