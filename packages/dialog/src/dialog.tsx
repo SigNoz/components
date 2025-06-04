6efa27efdae1c0944f841d8eee89b68cc480b072
Aditya Singh <adityasingh@Adityas-MacBook-Pro.local>
2025-06-04 19:07:32 +0530

    chore: dialog top height fix

--- packages/dialog/src/dialog.tsx
+++ packages/dialog/src/dialog.tsx
@@ -73,7 +73,7 @@ function DialogContent({
 			<DialogPrimitive.Content
 				data-slot="dialog-content"
 				className={cn(
-					'bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[80px] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] rounded-lg border border-vanilla-300 dark:border-slate-500 shadow-lg duration-200',
+					'bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[80px] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] gap-4 rounded-lg border shadow-lg duration-200 sm:max-w-[425px] border-vanilla-300 dark:border-slate-500',
 					widthClass,
 					className,
 				)}
