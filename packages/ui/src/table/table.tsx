7736908592c5145ea2fb68d3f889da7e9bf68766
Vinícius Lourenço <contact@viniciusl.com.br>
2026-03-05 16:28:43 -0300

    refactor(table): migrate from Tailwind to CSS modules

--- packages/ui/src/table/table.tsx
+++ packages/ui/src/table/table.tsx
@@ -2,6 +2,7 @@ import './index.css';
 import type * as React from 'react';
 
 import { cn } from '../lib/utils.js';
+import styles from './table.module.css';
 
 type TableProps = React.ComponentProps<'table'> & {
 	fixedHeight?: string | number;
@@ -15,7 +16,7 @@ function Table({ className, fixedHeight, containerRef, containerProps, ...props 
 			ref={containerRef}
 			data-slot="table-container"
 			className={cn(
-				'relative w-full overflow-x-auto',
+				styles['table-wrapper'],
 				fixedHeight && 'overflow-y-auto table-scroll-container sticky-header-table-container',
 				containerProps?.className
 			)}
@@ -34,11 +35,7 @@ function Table({ className, fixedHeight, containerRef, containerProps, ...props 
 		>
 			<table
 				data-slot="table"
-				className={cn(
-					'w-full caption-bottom text-sm',
-					fixedHeight && 'sticky-header-table',
-					className
-				)}
+				className={cn(styles['table'], fixedHeight && 'sticky-header-table', className)}
 				{...props}
 			/>
 		</div>
@@ -54,6 +51,7 @@ function TableHeader({
 		<thead
 			data-slot="table-header"
 			className={cn(
+				styles['table__header'],
 				'[&_tr]:border-b z-10 bg-base-black text-white',
 				sticky && 'sticky-header',
 				className
@@ -67,7 +65,7 @@ function TableBody({ className, ...props }: React.ComponentProps<'tbody'>) {
 	return (
 		<tbody
 			data-slot="table-body"
-			className={cn('[&_tr:last-child]:border-0', className)}
+			className={cn(styles['table__body'], '[&_tr:last-child]:border-0', className)}
 			{...props}
 		/>
 	);
@@ -77,23 +75,14 @@ function TableFooter({ className, ...props }: React.ComponentProps<'tfoot'>) {
 	return (
 		<tfoot
 			data-slot="table-footer"
-			className={cn('bg-muted/50 border-t font-medium [&>tr]:last:border-b-0', className)}
+			className={cn(styles['table__footer'], '[&>tr]:last:border-b-0', className)}
 			{...props}
 		/>
 	);
 }
 
 function TableRow({ className, ...props }: React.ComponentProps<'tr'>) {
-	return (
-		<tr
-			data-slot="table-row"
-			className={cn(
-				'hover:bg-muted/50 data-[state=selected]:bg-muted border-b transition-colors',
-				className
-			)}
-			{...props}
-		/>
-	);
+	return <tr data-slot="table-row" className={cn(styles['table__row'], className)} {...props} />;
 }
 
 function TableHead({ className, ...props }: React.ComponentProps<'th'>) {
@@ -101,7 +90,8 @@ function TableHead({ className, ...props }: React.ComponentProps<'th'>) {
 		<th
 			data-slot="table-head"
 			className={cn(
-				'text-foreground h-10 px-2 text-left align-middle font-medium whitespace-nowrap [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]',
+				styles['table__head'],
+				'text-foreground whitespace-nowrap [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]',
 				className
 			)}
 			{...props}
@@ -114,7 +104,8 @@ function TableCell({ className, ...props }: React.ComponentProps<'td'>) {
 		<td
 			data-slot="table-cell"
 			className={cn(
-				'p-2 align-middle whitespace-nowrap [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]',
+				styles['table__cell'],
+				'whitespace-nowrap [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]',
 				className
 			)}
 			{...props}
@@ -126,7 +117,7 @@ function TableCaption({ className, ...props }: React.ComponentProps<'caption'>) 
 	return (
 		<caption
 			data-slot="table-caption"
-			className={cn('text-muted-foreground mt-4 text-sm', className)}
+			className={cn(styles['table__caption'], className)}
 			{...props}
 		/>
 	);
