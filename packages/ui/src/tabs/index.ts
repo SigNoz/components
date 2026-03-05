949c1b8a4b43fdd0b3d7eb9a1aaa30adcdf7b456
Vinícius Lourenço <contact@viniciusl.com.br>
2026-03-05 16:18:41 -0300

    refactor(tabs): migrate from Tailwind to CSS modules

--- packages/ui/src/tabs/index.ts
+++ packages/ui/src/tabs/index.ts
@@ -1,8 +1,2 @@
 export type * from './tabs.js';
 export { default as Tabs } from './tabs.js';
-export {
-	tabsListVariants,
-	tabsListWrapperVariants,
-	tabsTriggerVariants,
-	tabsVariants,
-} from './tabVariants.js';
