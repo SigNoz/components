94d4b67d61e9e864395663ef11a0643a73a1f559
Vinícius Lourenço <contact@viniciusl.com.br>
2026-03-10 17:28:04 -0300

    feat(css-modules): migrate input

--- packages/ui/src/lib/utils.ts
+++ packages/ui/src/lib/utils.ts
@@ -4,3 +4,6 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
+
+// https://github.com/sindresorhus/type-fest/blob/main/source/simplify.d.ts
+export type Simplify<T> = { [KeyType in keyof T]: T[KeyType] } & {};
