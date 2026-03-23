ad2593408cf20b07fcf4c287cd4f94eb5feef65e
Vinícius Lourenço <contact@viniciusl.com.br>
2026-03-23 09:00:59 -0300

    feat(css-modules): migrate pin list

--- packages/ui/src/pin-list/index.ts
+++ packages/ui/src/pin-list/index.ts
@@ -1,5 +1,1 @@
-/**
- * @experimental Not migrated to CSS modules and JSDoc. Prefer other components until migration is complete.
- */
-export type * from './pin-list.js';
-export { PinList } from './pin-list.js';
+export { PinList, type PinListItem, type PinListProps } from './pin-list.js';
