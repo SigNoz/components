848b530e39c1cb8b5cd60d08a79aa3204c9a7912
manika-signoz <manika@signoz.io>
2026-05-25 16:55:35 +0530

    refactor(avatar): use module.scss instead of module.css

--- packages/ui/src/avatar/avatar.tsx
+++ packages/ui/src/avatar/avatar.tsx
@@ -1,6 +1,6 @@
 import { forwardRef, useState } from 'react';
 import { cn } from '../lib/utils.js';
-import styles from './avatar.module.css';
+import styles from './avatar.module.scss';
 
 export type AvatarSize = 'sm' | 'md' | 'lg' | 'xl';
 
